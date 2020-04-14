593e18782737c01fd28e570fc49925f1240aaad0
sthysel <sthysel@gmail>
2020-04-14 22:41:06 +0800

    set storage driver

--- balena-build.sh
+++ balena-build.sh
@@ -3,3 +3,4 @@
 RELEASE=warrior
 
 docker run --rm -it --privileged -v $(pwd)/build/balena-intel:/home/build balena-builder:$RELEASE ./balena-yocto-scripts/build/barys --rm-work -d --machine genericx86-64
+#docker run --rm -it --privileged -v $(pwd)/build/balena-intel:/home/build balena-builder:$RELEASE ./balena-yocto-scripts/build/barys --rm-work -d --machine surface-go
